87cbca297204bb2cef437bae2c5f7ff70185729c
Andrii Karpovich <a.karpovich@h24.ua>
2026-02-08 13:02:01 +0200

    calendar comp update

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,9 +1,10 @@
-import { CommonModule, DatePipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import {CommonModule, DatePipe} from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { EventScheduleComponent } from '../events-schedule/event-schedule.component';
 import { ActivatedRoute } from '@angular/router';
+import { MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-calendar',
@@ -13,19 +14,25 @@ import { ActivatedRoute } from '@angular/router';
     CommonModule,
     MatCardModule,
     EventScheduleComponent,
-    DatePipe
+    MatProgressBarModule,
+    DatePipe,
   ],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent {
   selectedDate: Date | null = null;
   route = inject(ActivatedRoute);
   currentRouteParams$ = this.route.params;
+  motivImageNumber: number = getRandomInt(4);
 
-  ngOnInit() {
+  constructor() {
     this.currentRouteParams$.subscribe((numericDate: { id?: string }) => {
-      if (numericDate['id']) this.selectedDate = new Date(numericDate['id'])
+      if (numericDate['id']) this.selectedDate = new Date(numericDate['id']);
     });
   }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
 }
